2ce49f4779f2b0416439c9dc45effa0556118c31
Cliff Harvey <cs.hbar@gmail.com>
2020-07-26 16:06:49 +0200

    Trim public exports from Servant.OpenAPI. Add module docstring.
    I believe toBareOpenAPI is likely to create confusion here, though it 
    may be helpful for testing.

--- servant-openapi/src/Servant/OpenAPI.hs
+++ servant-openapi/src/Servant/OpenAPI.hs
@@ -1,13 +1,9 @@
+-- | Provides the functionality for generating the 'OpenAPI' for a servant API. For examples see the Github README.
 module Servant.OpenAPI
   ( -- * Interface for generating OpenAPI definition
     HasOpenAPI
   , toOpenAPI
-  , toBareOpenAPI
-
   , blankInfo
-  , blankOpenAPI
-
-    -- * API Types
   ) where
 
 import Servant.OpenAPI.Internal
